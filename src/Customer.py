05d79aac77fe0f4726d98a08c332b0789e411ac4
prakashchconnect <prakashch.connect@gmail.com>
2024-05-25 22:22:25 +0530

    python project structure with modules

---
+++ src/Customer.py
@@ -0,0 +1,13 @@
+class Customer1 :
+    def __init__(self):
+        print('start of Customer1:init')
+    
+    def display_customer1(self):
+        print('start of Customer1:display_customer')
+
+class Customer2 :
+    def __init__(self):
+        print('start of Customer2:init')
+    
+    def display_customer2(self):
+        print('start of Customer2:display_customer')
